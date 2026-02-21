c7c8c463e896febb2e698745b104db19c433f55a
priyanshu1645 <141954346+priyanshu1645@users.noreply.github.com>
2026-02-21 16:18:39 +0530

    Time: 45 ms (27.24%), Space: 46.9 MB (83.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,14 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class Solution {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numToIndex = new HashMap<>();
+        int Arr[];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int [] {i , j};
+                }
 
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            
-            if (numToIndex.containsKey(complement)) {
-                return new int[] { numToIndex.get(complement), i };
             }
-            
-            numToIndex.put(nums[i], i);
         }
-        
-        throw new IllegalArgumentException("No two sum solution found");
+        return new int[] {};
     }
-
 }
-   
