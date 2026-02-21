8c33258df2c85fd0ca0473e5d5d462a30b0ef76a
priyanshu1645 <141954346+priyanshu1645@users.noreply.github.com>
2026-02-21 16:59:05 +0530

    updated
    added the new optimal approach

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,29 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int Arr[];
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int [] {i , j};
-                }
+        // below is optimal hashmap approach
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for (int i = 0; i < nums.length; i++) {
+
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[] { map.get(need), i };
             }
+
+            map.put(nums[i], i);
         }
+
         return new int[] {};
+        // below is brute force 2 pointer approach
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i + 1; j < nums.length; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             return new int [] {i , j};
+        //         }
+
+        //     }
+        // }
+        // return new int[] {};
     }
 }
